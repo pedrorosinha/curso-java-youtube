1dfc2badab90e5df595a391885d00effdf33c02d
pedro felipe <pedrofelipinho@gmail.com>
2024-05-14 17:58:38 -0300

    Código da aula 14

--- src/Aula11Ate20/NestedLoops.java
+++ src/Aula11Ate20/NestedLoops.java
@@ -1,4 +1,30 @@
 package Aula11Ate20;
 
+import java.util.Scanner;
+
 public class NestedLoops {
+    public static void main(String[] args) {
+        // nested loops = a loop inside of a loop
+
+        Scanner scanner = new Scanner(System.in);
+        int linhas, colunas;
+        String simbolo = "";
+
+        System.out.println("Digite a # de linhas: ");
+        linhas = scanner.nextInt();
+
+        System.out.println("Digite a # de colunas: ");
+        colunas = scanner.nextInt();
+
+        System.out.println("Digite o simbolo que vai usar: ");
+        simbolo = scanner.next();
+
+        for (int i = 1; i <= linhas; i++) {
+            System.out.println();
+            for (int j = 1; j <= colunas; j++) {
+                System.out.print(simbolo);
+            }
+        }
+
+    }
 }
